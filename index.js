dbf30b21db992a95b37fa07363f06ea001083994
isMbumbu <andrewambuka9@gmail.com>
2024-10-01 16:26:26 +0300

    Created nested functions

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(def='roller-skate',msg='This Saturday, I want to '){
+const unless= function(){
+    return `${msg}${def}!`;
+}
+return unless();
+}
+function mondayWork(def='go to the office.',msg= 'This Monday, I will'  ) {
+    const unless = function() {
+        return `${msg} ${def}`;
+    }
+    return unless();
+}
+function mondayWork(def = 'go to the office', msg = 'This Monday, I will ') {
+    return `${msg}${def}.`;
+}
+function wrapAdjective(wrapper) {
+    return function(adjective) {
+      return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+  }
+  
